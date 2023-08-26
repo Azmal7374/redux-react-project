41328fb792f6165762a712101d91dcc143b9564a
Azmal <azmalgazi8@gmail.com>
2023-08-26 23:24:22 +0600

    practice radux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import todoStore from './todosStore.js'
+// import todoStore from './todosStore.js'
+import store from './app/store.js'
+import CounterView from './app/features/counter/CounterView.jsx'
 // import store from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,8 +12,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   //   <App />
   // </Provider>,
 
-  <Provider store={todoStore}>
-    <App />
-  </Provider>,
+  // <Provider store={todoStore}>
+  //   <App />
+  // </Provider>,
+  <Provider store={store}>
+  <h2>Counter App</h2>
+   <CounterView></CounterView>
+</Provider>,
 
 )
